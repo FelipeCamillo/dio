b9745304dd0a3e17ed1d9c3d9a0f9db17385b4da
Felipe Camillo <160956741+FelipeCamillo@users.noreply.github.com>
2024-12-06 18:48:46 -0300

    Projeto finalizado

--- classificador nivel de heroi/desafio001.js
+++ classificador nivel de heroi/desafio001.js
@@ -1,21 +1,21 @@
-let nomeHeroi = "";
-let quantidadeXP = "";
+let nomeHeroi = "Felipe";
+let quantidadeXP = 10001;
+let nivel = "";
 if (quantidadeXP <= 1000){
-
-} else if(quantidadeXP > 1000 || quantidadeXP <= 2000){
-
-} else if(quantidadeXP > 2000 || quantidadeXP <= 5000){
-
-} else if(quantidadeXP > 5000 || quantidadeXP <= 7000){
-
-} else if(quantidadeXP > 7000 || quantidadeXP <= 8000){
-
-} else if(quantidadeXP > 8000 || quantidadeXP <= 9000){
-
-} else if(quantidadeXP > 9000 || quantidadeXP <=10000){
-
+    nivel = "Ferro";
+} else if(quantidadeXP > 1000 && quantidadeXP <= 2000){
+    nivel = "Bronze";
+} else if(quantidadeXP > 2000 && quantidadeXP <= 5000){
+    nivel = "Prata";
+} else if(quantidadeXP > 5000 && quantidadeXP <= 7000){
+    nivel = "Ouro";
+} else if(quantidadeXP > 7000 && quantidadeXP <= 8000){
+    nivel = "Platina";
+} else if(quantidadeXP > 8000 && quantidadeXP <= 9000){
+    nivel = "Ascendente";
+} else if(quantidadeXP > 9000 && quantidadeXP <=10000){
+    nivel = "Imortal";
 } else{
-
-}
-
-// qualquer parada
+    nivel = "Radiante";
+};
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}.`);
