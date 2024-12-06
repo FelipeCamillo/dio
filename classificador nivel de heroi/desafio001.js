f1bcefd776af15a47744dd0ea42010ad16ad0848
Felipe Camillo <160956741+FelipeCamillo@users.noreply.github.com>
2024-12-06 18:30:45 -0300

    Initial commit

---
+++ classificador nivel de heroi/desafio001.js
@@ -0,0 +1,21 @@
+let nomeHeroi = "";
+let quantidadeXP = "";
+if (quantidadeXP <= 1000){
+
+} else if(quantidadeXP > 1000 || quantidadeXP <= 2000){
+
+} else if(quantidadeXP > 2000 || quantidadeXP <= 5000){
+
+} else if(quantidadeXP > 5000 || quantidadeXP <= 7000){
+
+} else if(quantidadeXP > 7000 || quantidadeXP <= 8000){
+
+} else if(quantidadeXP > 8000 || quantidadeXP <= 9000){
+
+} else if(quantidadeXP > 9000 || quantidadeXP <=10000){
+
+} else{
+
+}
+
+// qualquer parada
